00f1943e83d927ad7100983d8c09c9be4e4d7ee1
Farmanian Karen <farmanian.k@yandex.ru>
2023-12-03 22:57:04 +0300

    hw-24-DI

---
+++ hw-24-DI/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -0,0 +1,142 @@
+package ru.otus.appcontainer;
+
+import org.reflections.Reflections;
+import ru.otus.appcontainer.api.AppComponent;
+import ru.otus.appcontainer.api.AppComponentsContainer;
+import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class AppComponentsContainerImpl implements AppComponentsContainer {
+
+    private final List<Object> appComponents = new ArrayList<>();
+    private final Map<String, Object> appComponentsByName = new HashMap<>();
+
+    public AppComponentsContainerImpl(Class<?>... initialConfigClass) {
+        processConfig(initialConfigClass);
+    }
+
+    public AppComponentsContainerImpl(String path) {
+        processConfigPath(path);
+    }
+
+    private void processConfigPath(String path) {
+        Reflections reflections = new Reflections(path);
+        Set<Class<?>> classes =  reflections.getSubTypesOf(Object.class);
+        Class<?>[] classArr = classes.toArray(new Class<?>[0]);
+        processConfig(classArr);
+//        classes.forEach(System.out::println);
+//        Store store = reflections.getStore();
+//        Collection<Map<String, Set<String>>> values = store.values();
+//        String[] mapValues = values.stream()
+//                .flatMap(map -> map.values().stream())
+//                .flatMap(Set::stream)
+//                .toArray(String[]::new);
+//        Class<?>[] classes = Arrays.stream(mapValues)
+//                .map(clazzStr -> {
+//                    var split = clazzStr.split("\\.");
+//                    return (Class<?>)split[split.length - 1] + ".class";
+//                })
+//                .toArray(Class[]::new);
+//        processConfig(classes);
+    }
+
+    private void processConfig(Class<?>... configClasses) {
+        Arrays.stream(configClasses).forEach(configClass -> {
+            checkConfigClass(configClass);
+            Object instance = this.getInstance(configClass);
+            var componentsWithOrder = getComponentsWithOrder(configClass);
+            putIntoAppContainers(componentsWithOrder, instance);
+        });
+    }
+
+    private void checkConfigClass(Class<?> configClass) {
+        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+        }
+    }
+
+    @Override
+    public <C> C getAppComponent(Class<C> componentClass) {
+        return (C) appComponents.stream()
+                .filter(component -> componentClass.isInstance(component))
+                .findFirst()
+                .orElseGet(null);
+    }
+
+    @Override
+    public <C> C getAppComponent(String componentName) {
+        return (C) appComponentsByName.get(componentName);
+    }
+
+    private Object getInstance(Class<?> configClass) {
+        try {
+            Constructor<?> constructor = configClass.getDeclaredConstructor();
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Map<Integer, List<Method>> getComponentsWithOrder(Class<?> configClass) {
+        Method[] methods = configClass.getMethods();
+        var appComponentOrder = new HashMap<Integer, List<Method>>();
+        Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .forEach(method -> {
+                    var annotation = method.getAnnotation(AppComponent.class);
+                    var key = annotation.order();
+                    if (appComponentOrder.containsKey(key)) {
+                        var value = appComponentOrder.get(key);
+                        value.add(method);
+                    } else {
+                        List<Method> methodList = new ArrayList<>();
+                        methodList.add(method);
+                        appComponentOrder.put(annotation.order(), methodList);
+                    }
+                });
+        return appComponentOrder;
+    }
+
+    private void putIntoAppContainers(Map<Integer, List<Method>> componentsWithOrder, Object instance) {
+        IntStream.range(0, componentsWithOrder.size())
+                .forEach(i ->
+                        componentsWithOrder.get(i).forEach(method -> {
+                            Class<?>[] types = method.getParameterTypes();
+                            Object[] objects = Arrays.stream(types)
+                                    .map(type -> appComponents.stream()
+                                            .filter(type::isInstance)
+                                            .findFirst()
+                                            .orElse(null))
+                                    .toArray();
+                            try {
+                                var object = method.invoke(instance, objects);
+                                verifyComponentIsNotAlreadyExist(object)
+                                ;
+                                appComponents.add(object);
+                                var annotation = method.getAnnotation(AppComponent.class);
+                                verifyComponentByNameIsNotAlreadyExist(annotation.name());
+                                appComponentsByName.put(annotation.name(), object);
+                            } catch (IllegalAccessException | InvocationTargetException e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                );
+    }
+
+    private void verifyComponentIsNotAlreadyExist(Object object) {
+        if (appComponents.contains(object))
+            throw new RuntimeException("Component (" + object.getClass() + ") already exist");
+    }
+
+    private void verifyComponentByNameIsNotAlreadyExist(String name) {
+        if (appComponentsByName.containsKey(name))
+            throw new RuntimeException("Component with name (" + name + ") already exist");
+    }
+
+}
